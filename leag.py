19b9c684bc3f0c7fa09ebec25b79faec130e30f5
aginga <brianaginga35@gmail.com>
2022-09-23 16:59:00 +0300

    new code

--- leag.py
+++ leag.py
@@ -1,1 +1,18 @@
 #EPL
+
+class PremierLeague:
+
+    @staticmethod
+    def list_of_the_teams():
+        names = list()
+        n = int(input('Enter the length of the teams : '))
+        for i in range(n):
+            x = input('Enter the teams: ')
+            names.append(x)
+
+
+pre = PremierLeague()
+pre.list_of_the_teams()
+
+
+
